c953b76fa54f7fe0ff8bb40a240b00ec6a42c12e
Shubham12051999 <shubham.navale12@gmail.com>
2020-09-04 00:48:10 +0530

    make it better

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post('/bookmark/new', (req, res) => {
     var Title = req.body.Title
     var Publisher = req.body.Publisher
     var Tags = req.body.Tags
-    if(Tags!=undefined) Tags=Tags.split(',')
+    if (Tags != undefined) Tags = Tags.split(',')
     var Creation_Time = Date()
     var Updation_Time = Date()
     var doc = { Link, Title, Publisher, Tags, Creation_Time, Updation_Time }
@@ -43,7 +43,7 @@ app.post('/bookmark/new', (req, res) => {
                                 }
                                 else {
                                     //updation is sucessful
-                                     console.log(result)
+                                    console.log(result)
                                 }
                             })
                         }
@@ -54,23 +54,23 @@ app.post('/bookmark/new', (req, res) => {
                     });
                 });
             }
-            return res.json(result)  
+            return res.json(result)
         }
     });
 });
 
 //delete bookmark
-app.delete('/bookmark/:Link',(req,res)=>{
-    var Link=req.params.Link //getting link of bookmark which we want to delete
-    if(Link!=undefined){
-        bookmarks.deleteOne({'Link':Link},(err,result)=>{ //delete bookmark from collection
-            if(err!=null){
+app.delete('/bookmark/:Link', (req, res) => {
+    var Link = req.params.Link //getting link of bookmark which we want to delete
+    if (Link != undefined) {
+        bookmarks.deleteOne({ 'Link': Link }, (err, result) => { //delete bookmark from collection
+            if (err != null) {
                 console.log(err)
                 res.send('Please try again later') // server error link mongo connection
             }
-            else{
+            else {
                 console.log(result)
-                if(result.deletedCount==0)
+                if (result.deletedCount == 0)
                     res.send('bookmark not found') //bookmark not present
                 else
                     res.send('bookmark deleted sucessfully') //bookmark deleted sucessfully
@@ -80,26 +80,26 @@ app.delete('/bookmark/:Link',(req,res)=>{
 })
 
 //Dispaly the all bookmarks present in database
-app.get('/bookmarks/all',(req,res)=>{
-    bookmarks.find({},(err,result)=>{
-        if(err!=null) return res.send('please try again sometime') //mongo server error
+app.get('/bookmarks/all', (req, res) => {
+    bookmarks.find({}, (err, result) => {
+        if (err != null) return res.send('please try again sometime') //mongo server error
         res.send(result) //list of all bookmarks
     })
 })
 
 //adding new tag to database
-app.post('/tag/new',(req,res)=>{
-    var Title=req.body.Title
+app.post('/tag/new', (req, res) => {
+    var Title = req.body.Title
     var Creation_Time = Date()
     var Updation_Time = Date()
-    var doc={Title,Creation_Time,Updation_Time}//creating doc as per the schema
-    var tag=new tags(doc)
-    tag.save((err,result)=>{
-        if(err!=null){
+    var doc = { Title, Creation_Time, Updation_Time }//creating doc as per the schema
+    var tag = new tags(doc)
+    tag.save((err, result) => {
+        if (err != null) {
             console.log(err)
             return res.send('tag already present')
         }
-        else{
+        else {
             //tag is sucessfully stored in database
             console.log(result)
             return res.send(result)
@@ -108,78 +108,79 @@ app.post('/tag/new',(req,res)=>{
 })
 
 //deleting the specific tag from database
-app.delete('/tag/:Title',(req,res)=>{
-    var Title=req.params.Title
-    tags.deleteOne({Title:Title},(err,result)=>{
-        if(err!=null) return res.send('please try again sometime') //server error
-        if(result.deletedCount==0) return res.send('Tag not present in database') //tag not present
+app.delete('/tag/:Title', (req, res) => {
+    var Title = req.params.Title
+    tags.deleteOne({ Title: Title }, (err, result) => {
+        if (err != null) return res.send('please try again sometime') //server error
+        if (result.deletedCount == 0) return res.send('Tag not present in database') //tag not present
         else return res.send('tag deleted sucesfully') //deletion sucessful.
     })
 })
 
 //adding the tag on bookmark
-app.put('/tag/:Link/:Title',(req,res)=>{
-    var Link=req.params.Link
-    var Title=req.params.Title
-    bookmarks.updateOne({Link:Link},{$addToSet:{Tags:Title}},{upsert:true},(err,result)=>{//upadting the document if new tag is present
-        console.log(err,result)
-        if(err!=null) return res.send('please try again some time')
-        else{
-            if(result.nModified>0){ 
-                bookmarks.updateOne({Link:Link},{$set:{Updation_Time:Date()}},{upsert:true},(err,result)=>{
-                    if(err!=null) return res.send('plaese try again some time') //server error
-                    if(result.nModified>0){
-                
-                var tag=new tags({
-                    Title:Title,
-                    Updation_Time:Date(),
-                    Creation_Time:Date()
-                }) //created the new tag if not present in database
-                tag.save((err,resu)=>{
-                    if(err!=null){
-                        tags.updateOne({ Title: Title }, { $set: { 'Updation_Time': Date() } }, { upsert: true }, (err, result) => {//if tag is present then updating
+app.put('/tag/:Link/:Title', (req, res) => {
+    var Link = req.params.Link
+    var Title = req.params.Title
+    bookmarks.updateOne({ Link: Link }, { $addToSet: { Tags: Title } }, { upsert: true }, (err, result) => {//upadting the document if new tag is present
+        console.log(err, result)
+        if (err != null) return res.send('please try again some time')
+        else {
+            if (result.nModified > 0) {
+                bookmarks.updateOne({ Link: Link }, { $set: { Updation_Time: Date() } }, { upsert: true }, (err, result) => {//updating the updatetime
+                    if (err != null) return res.send('plaese try again some time') //server error
+                    if (result.nModified > 0) {
+
+                        var tag = new tags({
+                            Title: Title,
+                            Updation_Time: Date(),
+                            Creation_Time: Date()
+                        }) //created the new tag if not present in database
+                        tag.save((err, resu) => {
                             if (err != null) {
-                                return res.send('please try again some time')
+                                tags.updateOne({ Title: Title }, { $set: { 'Updation_Time': Date() } }, { upsert: true }, (err, result) => {//if tag is present then updating
+                                    if (err != null) {
+                                        return res.send('please try again some time')
+                                    }
+                                    else {
+                                        //updation is sucessful
+                                        console.log(result)
+                                    }
+                                })
                             }
-                            else {
-                                //updation is sucessful
-                                 console.log(result)
-                            }
-                        }) 
+                            return res.send('document updated sucessfully')
+                        })
                     }
-                    return res.send('document updated sucessfully')
                 })
-            }})
             }
-            else{
+            else {
                 return res.send('document tag already present to bookmark')
             }
-    }
+        }
     })
 })
 
 //remove tag from bookmark
-app.delete('/tag/remove/:Link/:Title',(req,res)=>{
-    var Link=req.params.Link
-    var Title=req.params.Title
-    bookmarks.updateOne({Link:Link},{$pull:{Tags:Title}},{upsert:true},(err,result)=>{//removing tag from bookmark
+app.delete('/tag/remove/:Link/:Title', (req, res) => {
+    var Link = req.params.Link
+    var Title = req.params.Title
+    bookmarks.updateOne({ Link: Link }, { $pull: { Tags: Title } }, { upsert: true }, (err, result) => {//removing tag from bookmark
         console.log(result)
-        if(err!=null) return res.send('plaese try again some time') //server error
-        if(result.nModified>0) {
-            bookmarks.updateOne({Link:Link},{$set:{Updation_Time:Date()}},{upsert:true},(err,result)=>{
-                if(err!=null) return res.send('plaese try again some time') //server error
-                if(result.nModified>0) return res.send('Tag sucessfully removed from bookmark') //tag removal sucessful
+        if (err != null) return res.send('plaese try again some time') //server error
+        if (result.nModified > 0) {
+            bookmarks.updateOne({ Link: Link }, { $set: { Updation_Time: Date() } }, { upsert: true }, (err, result) => {//updating the updatetime
+                if (err != null) return res.send('plaese try again some time') //server error
+                if (result.nModified > 0) return res.send('Tag sucessfully removed from bookmark') //tag removal sucessful
             })
-            
+
         }
         else return res.send('Tag is not present on bookmark') //tag not prsent in bookmark
     })
 })
 
 //Display the all tags from database
-app.get('/tag/all',(req,res)=>{
-    tags.find({},(err,result)=>{
-        if(err!=null) return res.send('please try again sometime') //mongo server error
+app.get('/tag/all', (req, res) => {
+    tags.find({}, (err, result) => {
+        if (err != null) return res.send('please try again sometime') //mongo server error
         res.send(result) //list of all bookmarks
     })
 })
